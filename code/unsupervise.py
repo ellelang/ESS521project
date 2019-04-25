68d26188d7622fd70baa1c4212f7c32472cd73bb
Elle Lang <langzx@uw.edu>
2019-04-25 09:25:57 -0700

    WCMOga

---
+++ code/unsupervise.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+data_folder = Path('C:/Users/langzx/Desktop/github/ESS521project/data')
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from geopandas import GeoSeries, GeoDataFrame
+import itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
